113fc68651d07622c68fd857751c231f4d11131f
Shubhankar Deshmukh <56350246+shubh-git-bit@users.noreply.github.com>
2024-10-16 23:31:01 +0530

    Time: 23 ms (49.40%) | Memory: 26.6 MB (95.40%) - LeetSync

---
+++ 258-add-digits/add-digits.cs
@@ -0,0 +1,17 @@
+public class Solution 
+{
+    public int AddDigits(int num) 
+    {
+        if (num < 10)
+            return num;
+
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return AddDigits(sum);
+    }
+
+}
