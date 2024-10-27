d9560955b9a9994e78f53d3ba1ece64018ba2073
Shubhankar Deshmukh <56350246+shubh-git-bit@users.noreply.github.com>
2024-10-27 20:02:20 +0530

    Time: 250 ms (5.59%) | Memory: 109.9 MB (6.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -2,10 +2,15 @@ public class Solution
 {
     public int Fib(int n) 
     {
+        Dictionary<int,int> memo = new Dictionary<int,int>();
         if (n == 0)
             return 0;
         if (n == 1)
             return 1;
-        return Fib(n - 1) + Fib(n -2);   
+        if (memo.ContainsKey(n))
+            return memo[n];
+
+        memo[n] = Fib(n - 1) + Fib(n -2);   
+        return memo[n];
     }
 }
