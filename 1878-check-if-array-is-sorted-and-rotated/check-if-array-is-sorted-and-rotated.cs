82df8b9819ae59d41a0a264b540fdb7af94f4e47
Shubhankar Deshmukh <56350246+shubh-git-bit@users.noreply.github.com>
2024-11-05 00:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.81%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cs
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cs
@@ -2,41 +2,15 @@ public class Solution
 {
     public bool Check(int[] nums) 
     {
-        int current = nums[0];
-        int index = 0;
+        int dips = 0;
         for (int i = 1; i < nums.Length; i++)
         {
-            if (current <= nums[i])
-                current = nums[i];
-            else
-            {
-                index = i;
-                break;
-            }
-                
+            if (nums[i] < nums[i - 1])
+                dips++;
         }
 
-        if (index != 0)
-        {
-            int[] rotateArray = Rotate(nums, nums.Length - index);
-            current = rotateArray[0];
-            for (int i = 1; i < rotateArray.Length; i++)
-            {
-                if (current <= rotateArray[i])
-                    current = rotateArray[i];
-                else
-                    return false;
-            }
-
-            return current == rotateArray[^1];
-        }
-        return current == nums[^1];
-    }
-    public int[] Rotate(int[] nums, int pos)
-    {
-        int[] rotateArray = new int[nums.Length];
-        for (int i = 0; i < nums.Length; i++)
-            rotateArray[(i + pos) % nums.Length] = nums[i];
-        return rotateArray;
+        if (nums[nums.Length - 1] > nums[0])
+            dips++;
+        return dips < 2;
     }
 }
