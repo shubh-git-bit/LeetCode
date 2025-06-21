8386f436ec2d4f71e2da0f09947a261fe6aa1418
Shubhankar Deshmukh <56350246+shubh-git-bit@users.noreply.github.com>
2025-06-21 22:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (52.42%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cs
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cs
@@ -2,15 +2,19 @@ public class Solution
 {
     public bool Check(int[] nums) 
     {
-        int dips = 0;
-        for (int i = 1; i < nums.Length; i++)
+       int dips = 0;
+       if (nums.Length > 1)
+       {
+        for (int i = 1;i < nums.Length; i++)
         {
             if (nums[i] < nums[i - 1])
                 dips++;
         }
+       }
+       
+       if (nums[nums.Length - 1] > nums[0])
+        dips++;
 
-        if (nums[nums.Length - 1] > nums[0])
-            dips++;
-        return dips < 2;
+        return (dips <= 1); 
     }
 }
