d107b9c337c1678fcc161cb413ac6d2f75ea7cdf
Shubhankar Deshmukh <56350246+shubh-git-bit@users.noreply.github.com>
2026-05-07 01:12:22 +0530

    Time: 1 ms (97.15%) | Memory: 58.1 MB (65.79%) - LeetSync

--- 283-move-zeroes/move-zeroes.cs
+++ 283-move-zeroes/move-zeroes.cs
@@ -2,23 +2,18 @@ public class Solution
 {
     public void MoveZeroes(int[] nums) 
     {
-        int i = 0;
-        if (nums.Length > 1)
+        int slowPtr = 0;
+        for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = 0; j < nums.Length; j++)
+            if (nums[i] != 0)
             {
-                if (nums[j] != 0)
-                {
-                    Swap(nums, i, j);
-                    i++;
-                }
+                nums[slowPtr] = nums[i];
+                slowPtr++;
             }
         }
-    }
-    public void Swap(int[] nums, int a, int b)
-    {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+        for (int i = slowPtr; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
     }
 }
